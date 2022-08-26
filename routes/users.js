f82f4aeacca1185c49a907a2527ae10a7a3dd4b3
Nikhil Pandey <pandey.nikhil086@gmail.com>
2022-08-26 16:43:54 +0530

    Changes in users file

--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send({message:'Hello World user'});
+  res.send({message:'This is hello world!!'});
 });
 
 module.exports = router;
